6a0fb7d9e5a1653101ced98812ffb73a9b349ef7
Marcin Pestka <pestka.m.j@gail.com>
2023-08-30 00:13:39 +0200

    First custom collapsible

--- Web_App/src/components/CourseComponents/SideBar/SideBar.tsx
+++ Web_App/src/components/CourseComponents/SideBar/SideBar.tsx
@@ -1,14 +1,40 @@
-import List from "@mui/material/List";
-import LessonList from "./SideBarLessons";
+import { useState } from "react";
+import { courseStore } from "../../../store/courseStore";
+import { Observer } from "mobx-react-lite";
 
 export default function NestedList() {
+
+  function handleTopicClick(id:number) {
+    courseStore.activeCourseId = id;
+  }
+
+  function handleLectureClick(id:number) {
+    courseStore.activeLectureId = id;
+  }
+
   return (
-    <List
-      sx={{ width: "100%", maxWidth: 360, minWidth:360 }}
-      component="nav"
-      aria-labelledby="nested-list-subheader"
-    >
-      <LessonList></LessonList>
-    </List>
+    <Observer>
+      {() => (
+      <div style={{ borderStyle: "solid",maxWidth:'360px',minWidth:'360px' }}>
+        <>
+      {courseStore.course &&
+      courseStore.course.lectures.map((lecture,i)=>(
+        <div className="lectureSideBarElement" style={{ borderStyle: "solid", margin: "5px" }}>
+          <h5 onClick={()=>handleLectureClick(lecture.id)}>{(i+1)}. {lecture.title}</h5>
+          <div className={courseStore.activeLectureId === lecture.id ? "collapsingElement": "collapsingElement collapsed"}>
+            {lecture.topics.map((topic,j)=>(
+              <>
+                <p onClick={()=>handleTopicClick(topic.id)}>{(i+1)}.{(j+1)} {topic.title}</p>
+              </>
+            ))
+            }
+          </div>
+      </div>
+      ))
+    }
+        </>
+      </div>
+      )}
+    </Observer>
   );
 }
