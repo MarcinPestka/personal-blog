d0947bc7b4cc33b746f776a9fe62c09ad59a59b7
Marcin Pestka <pestka.m.j@gail.com>
2023-11-24 20:52:59 +0100

    refactor for a different package, added language

--- Web_App/src/components/Sections/CodeSection/CodeEditComponent.tsx
+++ Web_App/src/components/Sections/CodeSection/CodeEditComponent.tsx
@@ -1,14 +1,25 @@
-import { Grid, TextField } from "@mui/material";
+import { Autocomplete, Grid, TextField } from "@mui/material";
 import "highlight.js/styles/github.css";
 import { editingCourseStore } from "../../../store/editingCourseStore";
 import { sectionStore } from "../../../store/sectionStore";
 
 export function CodeEditComponent() {
+  const options = ['csharp', 'javascript'];
+
   return (
     <>
       <Grid container>
         <Grid item xs={10}>
-            <TextField fullWidth multiline rows={5} defaultValue={sectionStore.newSection ? sectionStore.newSection.subTitle:""} onChange={(e) => {sectionStore.newSection.subTitle = e.target.value}}/>
+        <Autocomplete
+            disablePortal
+            id="combo-box-demo"
+            options={options}
+            sx={{ width: 300 }}
+            value={sectionStore.newSection.text || null}
+            onChange={(event,newValue)=>{sectionStore.newSection.text = newValue!}}
+            renderInput={(params) => <TextField {...params} label="language" />}
+          />
+          <TextField fullWidth multiline rows={5} defaultValue={sectionStore.newSection ? sectionStore.newSection.subTitle:""} onChange={(e) => {sectionStore.newSection.subTitle = e.target.value}}/>
         </Grid>
       </Grid>
     </>
