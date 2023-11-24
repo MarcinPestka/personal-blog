767f032371092ae134e1b0c99ef6ec8e468cdba5
Marcin Pestka <pestka.m.j@gail.com>
2023-11-24 20:01:12 +0100

    markdown support for text section

--- Web_App/src/components/Sections/TextComponent/TextComponent.tsx
+++ Web_App/src/components/Sections/TextComponent/TextComponent.tsx
@@ -1,12 +1,13 @@
 import { Grid } from "@mui/material";
 import { ISection } from "../../../models/section.model";
+import ReactMarkdown from "react-markdown";
 
 export function TextComponent(props: ISection) {
   return (
     <>
       <Grid container>
         <Grid item xs={10}>
-          <div className="lineBreakDisplay">{props.text}</div>
+          <div className="lineBreakDisplay"><ReactMarkdown>{props.text}</ReactMarkdown></div>
         </Grid>
       </Grid>
     </>
